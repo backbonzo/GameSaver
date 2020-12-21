f1e637c8090f2ae351eded2c636970bafa94f4ad
Lemorz56 <sebastian@msbrg.net>
2020-12-21 21:07:32 +0100

    Added .env and added credentials

--- prototype/web-app/server/src/index.js
+++ prototype/web-app/server/src/index.js
@@ -2,17 +2,26 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config(); // Automatically read .env if exist
 
 // Moving middleware functions into their own file
 const middelwares = require('./middlewares');
+
+const app = express();
+
+mongoose.connect('process.env.DATABASE_URL', {
+  useNewUrlParser: true,
+});
+
 // Define app as express use
 // Morgan, Helmet and Cors with this.
-const app = express();
 app.use(morgan('common'));
 app.use(helmet());
 app.use(cors({
   // We let our app know that we will ONLY accept req from this url
-  origin: 'http://localhost:3000',
+  origin: process.env.CORS_ORIGIN,
 }));
 
 // Simple get for the / url
