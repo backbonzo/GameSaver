1e5440f62f887430800e507d4ac692b01a30267c
Lemorz56 <sebbe_xbox@hotmail.com>
2020-12-21 10:53:41 +0100

    Added basic server setup, will be working more on this part now

---
+++ prototype/web-app/server/src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+
+const app = express();
+app.use(morgan('common'));
+app.use(helmet());
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
