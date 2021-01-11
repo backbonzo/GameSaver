8e588186146b0181176442056ca57e545ac952d3
Lemorz56 <sebastian@msbrg.net>
2021-01-11 09:20:12 +0100

    Merge from gridfs branch and refactor code

--- prototype/web-app/server/src/index.js
+++ prototype/web-app/server/src/index.js
@@ -3,10 +3,6 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
 const mongoose = require('mongoose');
-// GRIDFS TEST REQS
-const crypto = require('crypto');
-const multer = require('multer');
-const GridFsStorage = require('multer-gridfs-storage');
 const Grid = require('gridfs-stream');
 
 require('dotenv').config(); // Automatically read .env if exist
@@ -15,7 +11,6 @@ require('dotenv').config(); // Automatically read .env if exist
 const middelwares = require('./middlewares');
 // Routing below
 const devices = require('./api/devices');
-/* const fileTransfer = require('./api/files'); */
 
 const app = express();
 
@@ -39,17 +34,7 @@ connection.once('open', () => {
   gfs = Grid(connection.db, mongoose.mongo);
   gfs.collection('fs');
 });
-/*
-let gfs;
-const conn = mongoose.createConnection(process.env.DATABASE_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).once('open', () => {
-  // Init stream
-  gfs = Grid(conn.db, mongoose.mongo);
-  gfs.collection('fs');
-});
-*/
+
 // Define app as express use
 // Morgan, Helmet and Cors with this.
 app.use(morgan('common'));
@@ -68,58 +53,23 @@ app.get('/', (req, res) => {
   });
 });
 
-/* app.use('api/files', fileTransfer); */
-
 // We use the router before the notFound since we want it to register
 // and because we want to use it AFTER our middlewares above
 app.use('/api/devices', devices);
 
-// Below is TEST 1, using gfs findOne which renders the picutre only partly not the whole chunk
-app.get('/file/:id', (req, res) => {
+// Below is fetch images,using gfs findOne which renders the picutre only partly not the whole chunk
+app.get('/file/:id', (req, res, next) => {
   const fileId = req.params.id;
   res.contentType('image/png');
-  gfs.files.findOne({ _id: fileId }, (files, err) => {
-    /* if (!file || file.length === 0) {
-      console.log(`FILE_MF: ${file}`);
-      return res.status(404).json({
-        err: 'Could not find what you were looking for',
+  gfs.files.findOne({ _id: fileId }, () => {
+    try {
+      const readstream = gfs.createReadStream({
+        _id: fileId,
       });
-    } */
-    // File exists
-    // return res.json(file);
-    // read output
-    // const readstream = gfs.createReadStream(file.id);
-    // readstream.pipe(res);
-    /* if (files.length < 0) {
-      console.log('Files was less then zero');
-      console.log(files.length);
-    } */
-    const readstream = gfs.createReadStream({
-      _id: fileId,
-    });
-    readstream.pipe(res);
-  });
-});
-
-// BELOW IS TEST 2, using gfs.FIND instead of FindOne(seems to not be supported anymore)
-app.get('/file1/:id', (req, res) => {
-  res.contentType('image/png');
-  gfs.find({ _id: req.params.id }).toArray((err, files) => {
-    // if files
-    if (!files || files.length === 0) {
-      // eslint-disable-next-line no-console
-      console.log('Files not found');
+      readstream.pipe(res);
+    } catch (error) {
+      next(error);
     }
-    const readstream = gfs.createReadStream({
-      _id: req.params.id,
-    });
-    readstream.on('data', (chunk) => {
-      res.write(chunk);
-    });
-    readstream.on('end', () => { // WAS HERE
-      res.end();
-    });
-    readstream.pipe(res);
   });
 });
 
